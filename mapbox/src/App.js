33bf9c23462485988534ef0ac714275ebfd9b1cd
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-04-30 16:58:16 -0400

    rebuilt from an example

--- mapbox/src/App.js
+++ mapbox/src/App.js
@@ -1,33 +1,75 @@
 import React, { useState } from 'react';
-import ReactMapGL, { Marker } from 'react-map-gl';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 import * as earthquake from './data/earthquakes.json';
-import { MapPin } from 'styled-icons/fa-solid/MapPin';
+import { MapMarker } from 'styled-icons/fa-solid/MapMarker';
+import { Moon } from 'styled-icons/boxicons-regular/Moon';
+import { Moon as Moon2 } from 'styled-icons/boxicons-solid/Moon';
 import styled from 'styled-components';
+import * as moment from 'moment';
 import './App.css';
 
 export default function App() {
-  const RedPin = styled(MapPin)`
+  const RedMarker = styled(MapMarker)`
     height: 20px;
     width: 20px;
     color: red;
   `;
 
+  const LightMoon = styled(Moon)`
+    height: 35px;
+    width: 35px;
+    color: black;
+  `;
+
+  const DarkMoon = styled(Moon2)`
+    height: 35px;
+    width: 35px;
+    color: black;
+  `;
+
   const [viewport, setViewport] = useState({
-    width: '800px',
-    height: '800px',
+    width: '1400px',
+    height: '650px',
     latitude: 37.7749,
     longitude: -122.4194,
     zoom: 8
   });
 
+  const [selectedQuake, setSelectedQuake] = useState(null);
+  const [darkmode, setDarkmode] = useState(null);
+
+  const isDarkmode = () => {
+    if (darkmode) {
+      setDarkmode(null);
+    } else {
+      setDarkmode(true);
+    }
+  };
+
   return (
     <div className="map">
-      <h1>Map</h1>
-      <p />
+      <div className="nav">
+        <h3>Demo 4/23/2019 </h3>
+        <p>
+          Data can be found{' '}
+          <a href="https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php">
+            here
+          </a>
+          .
+        </p>
+        <button className="night" onClick={isDarkmode}>
+          {darkmode ? <DarkMoon /> : <LightMoon />}
+        </button>
+      </div>
+
       <ReactMapGL
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        mapStyle="mapbox://styles/thirdeyeclub/cjutdopu33db01fmsym0vr1pi"
+        mapStyle={
+          darkmode
+            ? 'mapbox://styles/thirdeyeclub/cjv3e2kv60j5g1fqxc07ftk4f'
+            : 'mapbox://styles/thirdeyeclub/cjutdopu33db01fmsym0vr1pi'
+        }
         onViewportChange={viewport => {
           setViewport(viewport);
         }}
@@ -38,11 +80,30 @@ export default function App() {
             latitude={quake.geometry.coordinates[1]}
             longitude={quake.geometry.coordinates[0]}
           >
-            <button>
-              <RedPin />
+            <button
+              className="marker"
+              onClick={() => {
+                setSelectedQuake(quake);
+              }}
+            >
+              <RedMarker />
             </button>
           </Marker>
         ))}
+
+        {selectedQuake ? (
+          <Popup
+            latitude={selectedQuake.geometry.coordinates[1]}
+            longitude={selectedQuake.geometry.coordinates[0]}
+            onClose={() => {
+              setSelectedQuake(null);
+            }}
+          >
+            <h4>{selectedQuake.properties.place}</h4>
+            <p>Magnitude: {selectedQuake.properties.mag}</p>
+            <p>Time: {moment(selectedQuake.properties.time).fromNow()}</p>
+          </Popup>
+        ) : null}
       </ReactMapGL>
     </div>
   );
