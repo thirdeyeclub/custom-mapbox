aa4734ba914a0ca2e420b9924f8f0c17d90cf42c
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-05-01 23:08:54 -0400

    fix me

--- mapbox/src/App.js
+++ mapbox/src/App.js
@@ -22,7 +22,92 @@ export default function App() {
   });
 // cannot ge token to work for some reason
 const TOKEN = "pk.eyJj2";
+return (
+  <>
+        <div className="rpgui-content">
+        <nav className="framed rpgui-draggable">
+          <button href="#">Compare</button>
+        </nav>
+    <div className="fluid-container">
+      <div className="map">
+        <p />
+        <ReactMapGL
+          {...viewport}
+          mapboxApiAccessToken={TOKEN}
+          mapStyle="mapbox://styles/thirdeyeclub/cjv3e2kv60j5g1fqxc07ftk4f"
+          onViewportChange={viewport => {
+            setViewport(viewport);
+          }}
+        >
+          {earthquake.features.map(quake => (
+            <Marker
+              key={quake.id}
+              latitude={quake.geometry.coordinates[1]}
+              longitude={quake.geometry.coordinates[0]}
+            >
+              <button>
+                <RedPin />
+              </button>
+            </Marker>
+          ))}
+        </ReactMapGL>
+      </div>
+      <div className="side">
+        <div className="sideHigh">high</div>
+        <div className="sideLow">low</div>
+      </div>
+    </div>
+  </div>
+  </>return (
+<>
+      <div className="rpgui-content">
+      <nav className="framed rpgui-draggable">
+        <button href="#">Compare</button>
+      </nav>
+  <div className="fluid-container">
+    <div className="map">
+      <p />
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={TOKEN}
+        mapStyle="mapbox://styles/thirdeyeclub/cjv3e2kv60j5g1fqxc07ftk4f"
+        onViewportChange={viewport => {
+          setViewport(viewport);
+        }}
+      >
+        {earthquake.features.map(quake => (
+          <Marker
+            key={quake.id}
+            latitude={quake.geometry.coordinates[1]}
+            longitude={quake.geometry.coordinates[0]}
+          >
+            <button>
+              <RedPin />
+            </button>
+          </Marker>
+        ))}
+      </ReactMapGL>
+    </div>
+    <div className="side">
+      <div className="sideHigh">high</div>
+      <div className="sideLow">low</div>
+    </div>
+  </div>
+</div>
+</>
 
+
+
+
+
+(
+<>
+      <div className="rpgui-content">
+      <nav className="framed rpgui-draggable">
+        <button href="#">Compare</button>
+      </nav>
+  <div className="fluid-container">
+    <div className="map">
       <p />
       <ReactMapGL
         {...viewport}
